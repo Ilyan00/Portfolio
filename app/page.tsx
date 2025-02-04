3e0a3664ad1c208d1c7a61525340ef50a1f4b2b9
Ilyan-Jude Bain-Trimbach <ilyanjudecour@gmail.com>
2025-02-04 17:46:54 +0100

    fixe: scroll for phone

--- app/page.tsx
+++ app/page.tsx
@@ -8,20 +8,41 @@ import Ballon from "./Ballon";
 
 export default function Home() {
   const [scrollY, setScrollY] = useState(0);
+  const [touchStartY, setTouchStartY] = useState(0);
 
   useEffect(() => {
+    const controller = new AbortController();
     const handleWheel = (event: WheelEvent) => {
       setScrollY((prev) =>
         Math.min(Math.max(0, prev + event.deltaY * 0.5), 5390)
       );
     };
 
-    window.addEventListener("wheel", handleWheel);
+    const handleTouchStart = (event: TouchEvent) => {
+      setTouchStartY(event.touches[0].clientY);
+    };
+
+    const handleTouchMove = (event: TouchEvent) => {
+      const touchY = event.touches[0].clientY;
+      const deltaY = touchStartY - touchY;
+      setTouchStartY(touchY);
+      setScrollY((prev) => Math.min(Math.max(0, prev - deltaY * 1), 5390));
+    };
+
+    window.addEventListener("wheel", handleWheel, {
+      signal: controller.signal,
+    });
+    window.addEventListener("touchstart", handleTouchStart, {
+      signal: controller.signal,
+    });
+    window.addEventListener("touchmove", handleTouchMove, {
+      signal: controller.signal,
+    });
 
     return () => {
-      window.removeEventListener("wheel", handleWheel);
+      controller.abort();
     };
-  }, []);
+  }, [touchStartY]);
 
   const handleResetScroll = () => {
     setScrollY(0);
